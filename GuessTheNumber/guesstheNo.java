77a39752c85a32a33301d4016b16e76a2900a593
Harsh Raj <harshraj.work021@gmail.com>
2026-07-31 13:19:10 +0530

    Add Guess The Number Game

---
+++ GuessTheNumber/guesstheNo.java
@@ -0,0 +1,63 @@
+package GuessTheNumber;
+
+
+    import java.util.Random;
+import java.util.Scanner;
+
+class Game {
+
+    private int number;
+    private int userInput;
+    private int noOfGuesses = 0;
+
+    // Constructor
+    Game() {
+        Random random = new Random();
+        number = random.nextInt(100) + 1;
+    }
+
+    // Take user input
+    void takeUserInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Guess the number: ");
+        userInput = sc.nextInt();
+        noOfGuesses++;
+    }
+
+    // Check guess
+    boolean isCorrectNumber() {
+
+        if (userInput == number) {
+            System.out.println("Correct!");
+            return true;
+        } 
+        else if (userInput < number) {
+            System.out.println("Too low!");
+        } 
+        else {
+            System.out.println("Too high!");
+        }
+
+        return false;
+    }
+
+    int getNoOfGuesses() {
+        return noOfGuesses;
+    }
+}
+
+public class guesstheNo {
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+
+        while (!game.isCorrectNumber()) {
+            game.takeUserInput();
+        }
+
+        System.out.println("Guesses: " + game.getNoOfGuesses());
+    }
+}
+    
+
